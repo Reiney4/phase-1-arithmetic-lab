da43d8e4556f2bc25ab6156e912c3c89510f363f
Valerie <valeriejeptoo4@gmail.com>
2023-06-16 22:53:06 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+
+ const muliply = 62
+ 31*2  
+
+const  random  = 1
+    Number.isInteger;
+    true
+    
+
+const  mob = 4
+   67 / 9;
+
+    
+
+  const max  = 20
+   Math.max(11, 8, 20, 4);
+
+    
+   
+
